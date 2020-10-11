d13f3a88d1b44188e1906f2dd27ad093b6a913e0
ajg7 <ajgebara@gmail.com>
2020-10-10 20:12:56 -0500

    endpoints work, and were tested successfully in postman

--- server/routers/model-pokemonMembers.js
+++ server/routers/model-pokemonMembers.js
@@ -1,9 +1,23 @@
 const db = require("../data/db-config");
 
 module.exports = {
-    find
+    find,
+    findById,
+    add
 }
 
 function find() {
     return db("Pokemon Members")
+}
+
+function findById(id) {
+    return db("Pokemon Members").where("id", "=", id);
+}
+
+function add(pokemon) {
+    return db("Pokemon Members").insert(pokemon, "id")
+        .then(ids => {
+            const id = ids[0];
+            return db("Pokemon Members").where({ id }).first();
+        })
 }
--- server/routers/router-pokemonMembers.js
+++ server/routers/router-pokemonMembers.js
@@ -13,4 +13,29 @@ router.get("/", (request, response) => {
         })
 })
 
+router.get("/:id", (request, response) => {
+    PokemonMembers.findById(request.params.id)
+        .then(pokemon => {
+            if(pokemon) {
+                response.status(200).json(pokemon)
+            } else {
+                response.status(404).json({ message: error.message })
+            }
+        })
+        .catch(error => {
+            response.status(500).json({ error: error.message })
+        })
+})
+
+router.post("/", (request, response) => {
+    const pokemonData = request.body;
+    PokemonMembers.add(pokemonData)
+        .then(ids => {
+            response.status(201).json({ created: ids[0] })
+        })
+        .catch(error => {
+            response.status(500).json({ message: error.message })
+        })
+})
+
 module.exports = router;
