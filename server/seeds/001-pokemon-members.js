97c56686e1371342e14f107980904dfebce1dff3
ajg7 <ajgebara@gmail.com>
2020-10-11 11:01:53 -0500

    changed seed file

--- server/seeds/001-pokemon-members.js
+++ server/seeds/001-pokemon-members.js
@@ -1,7 +1,13 @@
 
 exports.seed = function(knex) {
   const pokemonMembers = [
-    
+    {
+      "Name": "missingno",
+      "Type1": "Normal",
+      "Type2": null,
+      "ImgUrl": "https://cdn.bulbagarden.net/upload/9/98/Missingno_RB.png",
+      "PokemonNumber": 152
+    }
   ]
   return knex("Pokemon Members").insert(pokemonMembers);
 };
