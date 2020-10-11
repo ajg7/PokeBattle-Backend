b7166398127821085d137ecd5cbdc3ebcf198fb4
ajg7 <ajgebara@gmail.com>
2020-10-10 23:56:07 -0500

    got rid of seed

--- server/seeds/001-pokemon-members.js
+++ server/seeds/001-pokemon-members.js
@@ -1,13 +1,7 @@
 
 exports.seed = function(knex) {
   const pokemonMembers = [
-    {
-      name: "test",
-      type1: "fire",
-      type2: null,
-      imgUrl: "",
-      PokemonNumber: 152
-    }
+    
   ]
   return knex("Pokemon Members").insert(pokemonMembers);
 };
