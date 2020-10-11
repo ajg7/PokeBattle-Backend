0a95403233349558342bd020338d12d3e1968153
ajg7 <ajgebara@gmail.com>
2020-10-10 21:01:40 -0500

    lowercased port in console log

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require("./server/api/server");
 const port = process.env.PORT || 7000;
 
 server.listen(port, () => {
-    console.log(`Listening on port ${PORT}...`);
+    console.log(`Listening on port ${port}...`);
 });
