722e56cb00fe0b3595d041dd735e147b9c142f4e
ajg7 <ajgebara@gmail.com>
2020-10-20 21:08:56 -0500

    added a logger middleware to pokemon endpoint

--- server/api/server.js
+++ server/api/server.js
@@ -4,9 +4,21 @@ const PokemonRouter = require("../routers/router-pokemon");
 
 
 const server = express();
+
+
+// Middleware
+
+const logger = (request, response, next) => {
+    console.log(`[${new Date().toISOString()}] ${request.method} to ${request.url} ${request.get("Origin")}`)
+    next();
+}
+
+
+
 server.use(helmet());
 server.use(express.json());
-server.use("/pokemon", PokemonRouter);
+server.use("/pokemon", logger, PokemonRouter);
+
 
 
 server.get("/", (request, response) => {
