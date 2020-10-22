13c54b9d685f9e1a1759336df13ce6256332c5c4
ajg7 <ajgebara@gmail.com>
2020-10-22 18:46:09 -0500

    set up the test for auth pokemon router

--- tests/server.test.js
+++ tests/server.test.js
@@ -2,15 +2,13 @@ const { expectCt } = require("helmet");
 const supertest = require("supertest");
 const server = require("../server/api/server");
 
-const credentials = {
-    "email": "1Mario&Luigi1",
-    "password": "Vulpix777"
-}
-
-let token;
-
-
 describe("pokemon exist in the pokemon table", () => {
+    const credentials = {
+        "email": "1Mario&Luigi1",
+        "password": "Vulpix777"
+    }
+    
+    let token;
     describe("authentication", () => {
         it("get token to access pokemon router", () => {
             return supertest(server).post("/login").send(credentials).then(response => {
@@ -28,6 +26,37 @@ describe("pokemon exist in the pokemon table", () => {
             return supertest(server).get(`/pokemon/${id}`).set("Authorization", token).then(response => {
                 expect(response.status).toBe(200), expect(response.body).toBeDefined();
             })
+        })
+    })
+})
+
+describe("authorized admins can post, put, or delete pokemon from table", () => {
+    const credentials = {
+        "email": "1Mario&Luigi1",
+        "password": "Vulpix777",
+        "isAdmin": true
+    }
+    
+    let token;
+
+    const pokemonToTest = {
+        "name": "missingno",
+        "type1": "normal",
+        "type2": null,
+        "imgURL": "https://vignette.wikia.nocookie.net/brokenpkmn/images/1/1a/MissingNo.jpg/revision/latest/scale-to-width-down/340?cb=20150308194339",
+        "number": 152,
+        "height": 9,
+        "weight": 1,
+        "entry": "A glitch in the matrix",
+        "habitat": "waters-edge",
+        "legendary": false,
+        "mythical": false,
+        "ancient": false
+    }
+
+    describe("authorized admins can get all pokemon", () => {
+        it("admins can get all pokemon through authPokemonRouter", () => {
+
         })
     })
 })
