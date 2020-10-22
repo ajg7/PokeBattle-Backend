8c7d3f3873eb5ef17be4677996b683a1986bf42c
ajg7 <ajgebara@gmail.com>
2020-10-22 18:21:04 -0500

    tested get by id endpoint for pokemon

--- tests/server.test.js
+++ tests/server.test.js
@@ -9,6 +9,7 @@ const credentials = {
 
 let token;
 
+
 describe("pokemon exist in the pokemon table", () => {
     describe("authentication", () => {
         it("get token to access pokemon router", () => {
@@ -19,6 +20,12 @@ describe("pokemon exist in the pokemon table", () => {
         })
         it("give me the list of pokemon", () => {
             return supertest(server).get("/pokemon").set("Authorization", token).then(response => {
+                expect(response.status).toBe(200), expect(response.body).toBeDefined();
+            })
+        })
+        it("give me a pokemon by its id", () => {
+            const id = 5;
+            return supertest(server).get(`/pokemon/${id}`).set("Authorization", token).then(response => {
                 expect(response.status).toBe(200), expect(response.body).toBeDefined();
             })
         })
