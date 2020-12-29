6be6bd02c02541255882a7ff1d763557a4bf2c24
ajg7 <ajgebara@gmail.com>
2020-12-28 19:55:07 -0600

    changed the model for pokemon-in-teams to have arrow functions

--- server/routers/pokemon_in_teams/model-pokemon_in_teams.js
+++ server/routers/pokemon_in_teams/model-pokemon_in_teams.js
@@ -1,14 +1,4 @@
-module.exports = {
-    add,
-    find,
-    findByUserId, 
-    getPokemonData,
-    update,
-    remove
-}
-
 const db = require("../../data/db-config");
-
 
 /*
 When a user clicks on "Make a New Team", then brings you to the pokemon cards, and adds the current id of the user to the teams table
@@ -17,32 +7,41 @@ The pokemon_in_teams table and pokemon table should JOIN to get access to the po
 When you add pokemon, it will add to the team row on the next page, where you can delete a pokemon
 */
 
-function add(pokemon_Id, team_Id) {
+const add = (pokemon_Id, team_Id) => {
     return db("pokemon_in_teams").insert({pokemon_Id: pokemon_Id, team_Id: team_Id});
 }
 
-function find(team_Id) {
+const find = team_Id => {
     return db("pokemon_in_teams").where({ team_Id });
 }
 
-function findByUserId(user_Id) {
+const findByUserId = user_Id => {
     return db("teams").where({ user_Id }).first();
 }
 
-function getPokemonData(team_Id) {
+const getPokemonData = team_Id => {
     return db("pokemon_in_teams")
         .join("pokemon", "pokemon_in_teams.pokemon_Id", "=", "pokemon.id")
         .select("*")
         .where({ team_Id })
 }
 
-function update(pokemon_Id, changes) {
+const update = (pokemon_Id, changes) => {
     //Might need to change back to the primary key
     return db("pokemon_in_teams").where({ pokemon_Id }).update(changes)
 }
 
-function remove(pokemon_Id) {
+const remove = pokemon_Id => {
     return db("pokemon_in_teams").where({ pokemon_Id }).del();
 }
 
 //Next need to create a function that will join pokemon_in_teams with pokemon table, so we can access the data
+
+module.exports = {
+    add,
+    find,
+    findByUserId, 
+    getPokemonData,
+    update,
+    remove
+}
