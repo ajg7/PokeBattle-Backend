782955bb84785c337d9616ecea8cf9f4bcabb894
ajg7 <ajgebara@gmail.com>
2021-01-17 11:32:17 -0600

    made crud ops file

---
+++ new/server/library/crudOps.js
@@ -0,0 +1,58 @@
+const { sendError } = require("./errorHandler");
+
+const getAll = async (response, query, name, ...args) => {
+	try {
+		const data = await query(...args);
+		response.status(200).json(data);
+	} catch (error) {
+		sendError(response, error, name);
+	}
+};
+
+const getById = async (response, query, name, ...args) => {
+	try {
+		const data = await query(...args);
+		if (!data) throw new Error("Not Found");
+		response.status(200).json(data[0]);
+	} catch (error) {
+		sendError(response, error, name);
+	}
+};
+
+const add = async (response, query, name, ...args) => {
+	try {
+		const data = await query(...args);
+		response.status(201).json(data);
+	} catch (error) {
+		sendError(response, error, name);
+	}
+};
+
+const update = async (response, query, name, ...args) => {
+	try {
+		const data = await query(...args);
+		console.log(data);
+		if (!data) throw new Error("Not Found");
+		response.status(204).end();
+	} catch (error) {
+		sendError(response, error, name);
+	}
+};
+
+const remove = async (response, query, name, ...args) => {
+	try {
+		const data = await query(...args);
+		if (!data) throw new Error("Not Found");
+		response.status(200).json(data);
+	} catch (error) {
+		sendError(response, error, name);
+	}
+};
+
+module.exports = {
+	getAll,
+	getById,
+	add,
+	update,
+	remove,
+};
