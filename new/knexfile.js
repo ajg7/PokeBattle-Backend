9160e31f4f47c03f2bd3dbf88104d57da43eb1a6
ajg7 <ajgebara@gmail.com>
2021-01-16 22:11:50 -0600

    made knex file

--- new/knexfile.js
+++ new/knexfile.js
@@ -1,40 +1,55 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
 module.exports = {
 	development: {
-		client: "sqlite3",
+		client: "pg",
 		connection: {
-			filename: "./dev.sqlite3",
+			host: "127.0.0.1",
+			user: "postgres",
+			password: "Romans8:37",
+			database: "pokebattle",
+			charset: "utf8",
+		},
+		migrations: {
+			directory: "./server/migrations",
+		},
+		seeds: {
+			directory: "./server/seeds",
 		},
 	},
 
 	staging: {
 		client: "postgresql",
 		connection: {
-			database: "my_db",
-			user: "username",
-			password: "password",
-		},
-		pool: {
-			min: 2,
-			max: 10,
+			database: "pokebattle",
+			user: "postgres",
+			password: "Romans8:37",
 		},
 		migrations: {
-			tableName: "knex_migrations",
+			directory: "./server/migrations",
+		},
+		seeds: {
+			directory: "./server/seeds",
 		},
 	},
 
 	production: {
 		client: "postgresql",
 		connection: {
-			database: "my_db",
-			user: "username",
-			password: "password",
+			database: "pokebattle",
+			user: "postgres",
+			password: "Romans8:37",
 		},
 		pool: {
 			min: 2,
 			max: 10,
 		},
 		migrations: {
-			tableName: "knex_migrations",
+			directory: "./server/migrations",
+		},
+		seeds: {
+			directory: "./server/seeds",
 		},
 	},
 };
