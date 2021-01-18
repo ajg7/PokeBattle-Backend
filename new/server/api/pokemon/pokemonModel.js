62c80a6f53941c0e7e149d39b0dca95676658411
ajg7 <ajgebara@gmail.com>
2021-01-18 14:12:43 -0600

    added some other Pokemon endpoints

--- new/server/api/pokemon/pokemonModel.js
+++ new/server/api/pokemon/pokemonModel.js
@@ -10,7 +10,25 @@ const getPokemonById = id => {
 	return db("pokemon").where({ id });
 };
 
+// Get Pokemon by their type
+const getPokemonByType = type => {
+	return db("pokemon").where({ type1: type }).orWhere({ type2: type }).orderBy("name");
+};
+
+// Get Pokemon in A - Z ordering
+const getPokemonAlphabetically = () => {
+	return db("pokemon").orderBy("name");
+};
+
+// Get Pokemon in Z - A ordering
+const getPokemonReverseAlphabetically = () => {
+	return db("pokemon").orderBy("name", "desc");
+};
+
 module.exports = {
 	getAllPokemon,
 	getPokemonById,
+	getPokemonByType,
+	getPokemonAlphabetically,
+	getPokemonReverseAlphabetically,
 };
--- new/server/api/pokemon/pokemonRouter.js
+++ new/server/api/pokemon/pokemonRouter.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { crudOps } = require("../../library");
+const { sendError } = require("../../library/errorHandler");
 
 const Pokemon = require("./pokemonModel");
 
@@ -10,6 +11,37 @@ router.get("/", (request, response) => {
 router.get("/:id", (request, response) => {
 	const { id } = request.params;
 	crudOps.getById(response, Pokemon.getPokemonById, "Specific Pokemon", id);
+});
+
+// Returns Pokemon by their Type
+router.get("/type/:type", async (request, response) => {
+	const { type } = request.params;
+	try {
+		const data = await Pokemon.getPokemonByType(type);
+		response.status(200).json(data);
+	} catch (error) {
+		sendError(response, error, "Pokemon By Type");
+	}
+});
+
+// Returns Pokemon Sorted Alphabetically (A - Z)
+router.get("/asc", async (request, response) => {
+	try {
+		const data = await Pokemon.getPokemonAlphabetically();
+		response.status(200).json(data);
+	} catch (error) {
+		sendError(response, error, "Pokemon Sorted Alphabetically");
+	}
+});
+
+// Returns Pokemon Sorted Reverse Alphabetically (Z - A)
+router.get("/desc", async (request, response) => {
+	try {
+		const data = await Pokemon.getPokemonReverseAlphabetically();
+		response.status(200).json(data);
+	} catch (error) {
+		sendError(response, error, "Pokemon Sorted Reverse Alphabetically");
+	}
 });
 
 module.exports = router;
