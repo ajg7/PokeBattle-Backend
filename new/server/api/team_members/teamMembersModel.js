35b39431dca079b353095d92ccef6d17d7ae7ff8
ajg7 <ajgebara@gmail.com>
2021-01-20 20:48:09 -0600

    fixed error

--- new/server/api/team_members/teamMembersModel.js
+++ new/server/api/team_members/teamMembersModel.js
@@ -15,11 +15,21 @@ const updateNickName = (team_Id, pokemon_Id, nickname) => {
 	return db("team_members").where({ team_Id, pokemon_Id }).update({ nickname });
 };
 
-const getPokemonData = team_Id => {
-	return db("team_members as TM")
-		.join("pokemon as P", "P.id", "=", "TM.pokemon_Id")
-		.where({ team_Id })
-		.limit(6);
+const getPokemonData = async user_Id => {
+	const map = new Map();
+	const resultQuery = await db("teams as T")
+		.leftJoin("team_members as TM", "TM.team_Id", "=", "T.id")
+		.leftJoin("pokemon as P", "P.id", "=", "TM.pokemon_Id")
+		.where({ user_Id });
+	for (const pokemon of resultQuery) {
+		if (map.get(pokemon.team_Id) === undefined) {
+			map.set(pokemon.team_Id, []);
+		}
+		map.get(pokemon.team_Id).push(pokemon);
+	}
+
+	console.log(map);
+	return map;
 };
 
 module.exports = {
