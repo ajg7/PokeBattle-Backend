447de58ab62170320284294f0671d5001a510e3b
ajg7 <ajgebara@gmail.com>
2021-01-24 12:35:43 -0600

    added a make a battle, update scores, and get scores model

--- server/api/battles/battleModel.js
+++ server/api/battles/battleModel.js
@@ -4,23 +4,29 @@ const getAllTypes = () => {
 	return db("pokemon").orderBy("type1").distinct("type1");
 };
 
-const makeABattle = (user_Id, team_Id) => {
-	return db("battles").insert({ user_Id, team_Id }).returning("id");
+const makeABattle = (user_Id, team_Id, player_score, challenger_score) => {
+	return db("battles").insert({ user_Id, team_Id, player_score, challenger_score }).returning("id");
 };
 
 const updateScores = (id, scores) => {
-	return db("battles").where({ id }).update(scores);
+	return db("battles").where({ id }).update(scores).returning("id");
 };
 
-const getScores = user_Id => {
+const getScoresByUserId = user_Id => {
 	return db("battles")
 		.where({ user_Id })
 		.select("id", "team_Id", "player_score", "challenger_score");
+};
+
+const getScoresByTeamId = team_Id => {
+	return db("battles")
+		.where({ team_Id});
 };
 
 module.exports = {
 	getAllTypes,
 	makeABattle,
 	updateScores,
-	getScores,
+	getScoresByUserId,
+	getScoresByTeamId
 };
--- server/api/battles/battleRouter.js
+++ server/api/battles/battleRouter.js
@@ -12,9 +12,9 @@ router.get("/types", async (request, response) => {
 });
 
 router.post("/", async (request, response) => {
-	const { userId, teamId } = request.body;
+	const { userId, teamId, playerScore, challengerScore } = request.body;
 	try {
-		const battleId = await Battle.makeABattle(userId, teamId);
+		const battleId = await Battle.makeABattle(userId, teamId, playerScore, challengerScore);
 		response.status(201).json(battleId);
 	} catch (error) {
 		sendError(response, error, "Make a Battle");
@@ -25,8 +25,8 @@ router.put("/:id", async (request, response) => {
 	const { id } = request.params;
 	const scores = request.body;
 	try {
-		await Battle.updateScores(id, scores);
-		response.status(204).end();
+		const battleId = await Battle.updateScores(id, scores);
+		response.status(204).json(battleId);
 	} catch (error) {
 		sendError(response, error, "Update Scores");
 	}
@@ -35,10 +35,21 @@ router.put("/:id", async (request, response) => {
 router.get("/user", async (request, response) => {
 	const { userId } = request.query;
 	try {
-		const scores = await Battle.getScores(userId);
+		const scores = await Battle.getScoresByUserId(userId);
 		response.status(200).json(scores);
 	} catch (error) {
 		sendError(response, error, "Get Scores");
+	}
+});
+
+router.get("/battles/:teamId", async (request, response) => {
+	const { teamId } = request.params;
+	try {
+		const data = await Battle.getScoresByTeamId(teamId);
+		response.status(200).json(data);
+	}
+	catch (error) {
+		sendError(response, error, "Get Scores By Team Id");
 	}
 });
 
