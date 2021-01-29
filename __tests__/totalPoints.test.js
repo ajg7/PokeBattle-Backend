7f1fd4fa65c978911d03b35e22a8754bf65a3c72
ajg7 <ajgebara@gmail.com>
2021-01-29 00:14:33 -0600

    added tests to the total points column in Users tabls

---
+++ __tests__/totalPoints.test.js
@@ -0,0 +1,12 @@
+const supertest = require("supertest");
+const server = require("../server/api/server");
+
+describe("Total Points", () => {
+    test("Get Total Points", () => {
+        return supertest(server).get(`/users/points/${22}`).then(response => expect(response.status).toBe(200));
+    });
+    test("Update Total Points", () => {
+        const updatedPoints = {points: 5};
+        return supertest(server).put(`/users/points/${22}`).send(updatedPoints).then(response => expect(response.status).toBe(204));
+    });
+});
