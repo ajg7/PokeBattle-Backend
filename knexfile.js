b51e94363ba04d0e47e0297e8af8e14857081481
ajg7 <ajgebara@gmail.com>
2021-02-06 12:41:38 -0600

    added ssl=true in knex file

--- knexfile.js
+++ knexfile.js
@@ -36,7 +36,7 @@ module.exports = {
 
 	production: {
 		client: "pg",
-		connection: `${process.env.DATABASE_URL}?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory`,
+		connection: `${process.env.DATABASE_URL}?ssl=true`,
 		sslmode: require,
 		pool: {
 			min: 2,
