52ac079cd9cc28900ed05829ec64dadbccdafd9c
ajg7 <ajgebara@gmail.com>
2021-02-06 11:29:08 -0600

    got rid of crudOps

--- server/library/index.js
+++ server/library/index.js
@@ -1,9 +1,7 @@
-const crudOps = require("./crudOps");
 const isValid = require("./isValidChecker");
 const tokenHelper = require("./tokenHelper");
 
 module.exports = {
-	crudOps,
 	isValid,
 	tokenHelper,
 };
