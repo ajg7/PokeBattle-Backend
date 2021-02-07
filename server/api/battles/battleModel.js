9e18f676a65847b7f41280ede414bc5cce5525e2
ajg7 <ajgebara@gmail.com>
2021-02-07 12:16:57 -0600

    makeABattle endpoint refactored

--- server/api/battles/battleModel.js
+++ server/api/battles/battleModel.js
@@ -7,13 +7,17 @@ const getAllTypes = async () => {
 };
 
 const makeABattle = async (user_Id, team_Id, player_score, challenger_score) => {
+	const id = await client.query("SELECT id from battles ORDER BY id DESC");
+	const battlesId = id.rows[0].id + 1;
 	const query = {
-		text: "INSERT INTO battles ()"
+		text: "INSERT INTO battles VALUES ($1, $2, $3, $4, $5) RETURNING id",
+		values: [battlesId, user_Id, team_Id, player_score, challenger_score]
 	};
 	const data = await client.query(query);
-	return db("battles")
-		.insert({ user_Id, team_Id, player_score, challenger_score })
-		.returning("id");
+	return data.rows[0];
+	// return db("battles")
+	// 	.insert({ user_Id, team_Id, player_score, challenger_score })
+	// 	.returning("id");
 };
 
 const updateScores = async (id, scores) => {
