f1c9c8f9be0346ec218bc66ecff6a3d5b6907cf4
ajg7 <ajgebara@gmail.com>
2021-02-12 16:59:05 -0600

    contacted the table successfully

--- server/api/likes/likesModel.js
+++ server/api/likes/likesModel.js
@@ -2,7 +2,7 @@ const client = require("../../../db-config");
 
 const getAllLikes = async () => {
     const data = await client.query("SELECT * FROM likes");
-    return data;
+    return data.rows;
 };
 
 
